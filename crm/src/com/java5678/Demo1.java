00560ea6d62593c5f9605d528ebd0794e66582fc
xiaomage26 <1144527331@qq.com>
2020-05-02 07:21:51 +0800

    tianqi 这个组员更新了这个文件

--- crm/src/com/java5678/Demo1.java
+++ crm/src/com/java5678/Demo1.java
@@ -7,5 +7,6 @@ package com.java5678;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("crm 系统 ssm环境搭建完毕");
+        System.out.println("tianqi 这个组员更新了这个文件");
     }
 }
