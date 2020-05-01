7bd05d9a6e60b68c52457fd24531c3ed4c6838ec
xiaomage26 <1144527331@qq.com>
2020-05-02 07:13:46 +0800

    ssm环境搭建完毕

---
+++ crm/src/com/java5678/Demo1.java
@@ -0,0 +1,11 @@
+package com.java5678;
+
+/**
+ * @author 小马哥
+ * @site www.java5678.com
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("crm 系统 ssm环境搭建完毕");
+    }
+}
