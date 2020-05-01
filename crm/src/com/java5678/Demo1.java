63c83dcbb967798631835c12ef4626e367974734
xiaomage26 <1144527331@qq.com>
2020-05-02 07:25:59 +0800

    ssm环境搭建完毕

--- crm/src/com/java5678/Demo1.java
+++ crm/src/com/java5678/Demo1.java
@@ -8,5 +8,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("crm 系统 ssm环境搭建完毕");
         System.out.println("tianqi 这个组员更新了这个文件");
+        System.out.println("maliu组长 第三天改了这个文件，加了内容2");
     }
 }
